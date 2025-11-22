b7f9316453f11482689bf2209040a46cc5fe750e
Darwin <clxmr3@gmail.com>
2025-11-22 10:36:26 -0500

    changed from home :o

--- src/adasd.java
+++ src/adasd.java
@@ -2,5 +2,6 @@ public class adasd {
     public static void main(String[] args) {
         System.out.println("wow");
         System.out.println("its freezing");
+        System.out.println("yoo i did this from home!!");
     }
 }
