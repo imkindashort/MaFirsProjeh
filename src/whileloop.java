8d31b4499a9310f983d3842d4cd82bfd3cf4e81a
BT_1N3_11 <sample@bths.edu>
2025-11-24 09:49:07 -0500

    exploring while loops

---
+++ src/whileloop.java
@@ -0,0 +1,9 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+    }
+}
